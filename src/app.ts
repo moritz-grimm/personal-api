2a5a55eff0c8e2f03c65b3abeaa88c68e1276285
moritz-grimm <molumo@icloud.com>
2026-04-18 16:53:02 +0200

    reorder app.ts app.route() to mirror filesystem order; sort imports

--- src/app.ts
+++ src/app.ts
@@ -1,32 +1,32 @@
 import { Hono } from "hono";
+import { HTTPException } from "hono/http-exception";
 import { corsMiddleware } from "./middleware/cors.js";
 import { umami } from "./middleware/umami.js";
-import info from "./routes/info.js";
-import github from "./routes/github.js";
-import api from "./routes/api.js";
 import fourhundredeighteen from "./routes/418/index.js";
-import { impressum } from "./routes/impressum.js";
-import { status } from "./routes/status/status.js";
-import { privacyPolicy } from "./routes/privacy-policy.js";
-import { lastUpdated } from "./routes/last-updated.js";
 import algorithms from "./routes/algorithms/algorithms.js";
-import { HTTPException } from "hono/http-exception";
+import api from "./routes/api.js";
+import github from "./routes/github.js";
+import { impressum } from "./routes/impressum.js";
+import info from "./routes/info.js";
+import { lastUpdated } from "./routes/last-updated.js";
+import { privacyPolicy } from "./routes/privacy-policy.js";
+import { status } from "./routes/status/status.js";
 
 const app = new Hono();
 
 app.use("*", corsMiddleware);
 app.use("*", umami);
 
-app.route("/", api);
-app.route("/info", info);
-app.route("/impressum", impressum);
-app.route("/privacy-policy", privacyPolicy);
-app.route("/github", github);
-app.route("/status", status);
-app.route("/last-updated", lastUpdated);
 app.route("/418", fourhundredeighteen);
 app.route("/fourhundredeighteen", fourhundredeighteen);
 app.route("/algorithms", algorithms);
+app.route("/status", status);
+app.route("/", api);
+app.route("/github", github);
+app.route("/impressum", impressum);
+app.route("/info", info);
+app.route("/last-updated", lastUpdated);
+app.route("/privacy-policy", privacyPolicy);
 
 app.onError((err, c) => {
     if (err instanceof HTTPException) {
